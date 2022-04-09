0f74c584be2ef4776ae809f373ae3dd9cd05dc83
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-04-09 20:23:01 +0400

    solutions of tasks form html academy

---
+++ html-academy/tasks/Function task 3.js
@@ -0,0 +1,38 @@
+let calculate = function(firstNumber, secondNumber, operator) {
+    let firstNum = Number(firstNumber);
+    let secondNum = Number(secondNumber);
+    let sum;
+    
+    if (operator === '-') {
+      sum = firstNum - secondNum;
+      }
+    if (operator === '+') {
+      sum = firstNum + secondNum;
+      }
+    if (operator === '*') {
+      sum = firstNum * secondNum;
+      }
+    if (operator === '/') {
+      sum = firstNum / secondNum;
+      }
+    return(sum);
+  }; 
+  
+  
+  
+  /* Техническое задание
+  
+  Напиши функцию calculate, которая будет распознавать пользовательский ввод, совершать математические операции и возвращать результат.
+  
+  Параметры функции — firstNumber, secondNumber и operator.
+  
+  Параметры firstNumber, secondNumber — операнды, параметр operator - оператор.
+  
+  Все значения приходят в виде строк, в том числе и операторы. В третьем параметре содержится один из операторов: сложение ('+'), вычитание ('-'), умножение ('*'), деление ('/').
+  
+  В зависимости от оператора программа должна совершать разные операции: складывать первое число со вторым, вычитать из первого числа второе, умножать первое на второе, делить первое на второе.
+  
+  Не забудь сделать из строк с числами настоящие числа.
+  
+  */
+  
