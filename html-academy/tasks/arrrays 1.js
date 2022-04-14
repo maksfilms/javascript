0c2ff5c567195a1fbb5dba9a6e0aff4e62f89900
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-04-14 23:38:25 +0400

    solution of a tasks from html academy

---
+++ html-academy/tasks/arrrays 1.js
@@ -0,0 +1,7 @@
+// understanding of usage varriable as index of array
+
+let usersByDay = [812, 1360, 657, 1247];
+let index = 0;
+console.log(usersByDay[index]);
+index = 3;
+console.log(usersByDay[index]);
