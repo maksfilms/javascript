69988b9c3c1962e19028c806f698e3e82a53465d
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-04-14 23:37:28 +0400

    solution of a tasks from html academy

---
+++ html-academy/tasks/arrrays length.js
@@ -0,0 +1,7 @@
+// understanding how to use .length
+
+let usersByDay = [812, 1360, 657, 1247, 1000];
+
+console.log(usersByDay.length);
+console.log(usersByDay[usersByDay.length - 1]);
+console.log(usersByDay[usersByDay.length - 3]);
